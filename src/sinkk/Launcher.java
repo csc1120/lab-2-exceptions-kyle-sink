81b90e2dbd463f89ffa02d1926dbe7c908ccdc53
sinkk <sinkk@msoe.edu>
2024-09-17 16:11:29 -0500

    Commit 6: An executable Lab2.jar file is created and placed at the root of my project

---
+++ src/sinkk/Launcher.java
@@ -0,0 +1,18 @@
+/*
+ * Course: CSC1020
+ * Lab 2 - Exceptions
+ * Launcher class
+ * Name: Kyle Sink
+ * Last Updated: 9/17/24
+ */
+package sinkk;
+
+/**
+ * Launches the program Driver
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        // Replace "Main" with the name of the class that extends Application
+        Driver.main(args);
+    }
+}
